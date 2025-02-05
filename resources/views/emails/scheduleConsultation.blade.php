72fcb0e040f78dc83d15a4419973c12e72b40e91
Tenghuy <tenghuyly35@gmailcom>
2025-02-05 16:02:47 +0700

    new revised

--- resources/views/emails/scheduleConsultation.blade.php
+++ resources/views/emails/scheduleConsultation.blade.php
@@ -6,7 +6,7 @@
     <p><strong>Name:</strong> {{ $ScheduleFormData['name'] }}</p>
     <p><strong>Phone Number:</strong> {{ $ScheduleFormData['phone_number'] }}</p>
     <p><strong>Email:</strong> {{ $ScheduleFormData['email'] }}</p>
-    <p><strong>Service Type:</strong> {{ $ScheduleFormData['date'] }}</p>
+    <p><strong>Date:</strong> {{ $ScheduleFormData['date'] }}</p>
     <p><strong>Message:</strong> {{ $ScheduleFormData['messages'] }}</p>
 
 </body>
